36191d0a0855311445fb3cad77b0cea75b1edf67
Abigail-Diaz <abigailsolano128@gmail.com>
2024-11-16 13:23:21 -0800

    Changes to be committed: Tested the data fetching from the API and test if the data is displayed with a displayData function 	modified:   cafe.html 	modified:   script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
+
 async function getData(){
     
-    const hot_drink_url = 'https://api.sampleapis.com/coffee/hot'
+    const hot_drink_url = 'https://api.sampleapis.com/coffee/hot';
 
     try {
         const response = await fetch(hot_drink_url);
@@ -9,10 +10,45 @@ async function getData(){
             throw new Error(`Response is not okay: Status of response is ${response.status}`);
         }
 
-            const json = await response.json();
-            console.log(json);  
+        // get the data
+            const data = await response.json();
+            console.log(data);
+            displayData(data)
         
         } catch (error){
         console.error(error.message);
     }
 }
+
+function displayData(data){
+    
+    const container = document.getElementById('data-container');
+    
+    let htmlContent = '';
+    
+    data.forEach(item => {
+        //const drink = document.createElement('div');
+        
+        // organize the data into corresponding variables
+        const title = item.title;
+        const description = item.description;
+        const ingredients = item.ingredients;
+        const imageUrl = item.image;
+
+        // create the html content to display the data in order
+        htmlContent += `
+        <div class = "drink"
+        <img src = "${imageUrl}" alt = "${title}">
+        <p><strong></strong> ${title}</p>
+        <p><strong>Description: </strong> ${description} </p>
+        <p><strong>Ingredients: </strong> ${ingredients} </p>
+        </div>`;
+    });
+
+    container.innerHTML = htmlContent;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    getData();
+});
+
